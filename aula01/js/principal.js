28fb2e76f0e499e347548e3e0a3a657388712569
Maison <maisongalvao144@gmail.com>
2020-04-18 08:50:33 -0400

    Arquivos da aula01

---
+++ aula01/js/principal.js
@@ -0,0 +1,6 @@
+alert("Olá mundo");
+var titulo = document.querySelector(".titulo");
+console.log("Mostrando no console", titulo);
+console.log(titulo.textContent)
+titulo.textContent = "Maison Marcel Clínica"
+console.log(document.querySelector("h1"))
